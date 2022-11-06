c80b66b6367d7b55f167b2cdaf98fe239c81a095
jakobgrine <18680782+jakobgrine@users.noreply.github.com>
2022-11-06 08:49:43 +0100

    Add config

---
+++ init.lua
@@ -0,0 +1,53 @@
+vim.opt.undofile = true
+vim.opt.ignorecase = true
+
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+
+vim.opt.mouse = 'a'
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.keymap.set('n', '<Leader>fs', '<Cmd>w<CR>')
+
+local ensure_packer = function()
+  local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+    vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+local packer_bootstrap = ensure_packer()
+
+require 'packer'.startup(function()
+  use 'wbthomason/packer.nvim'
+
+  use {'morhetz/gruvbox', config = function() require 'colorscheme' end}
+
+  use 'tpope/vim-surround'
+  use 'tpope/vim-repeat'
+  use 'tpope/vim-sleuth'
+  use {'tpope/vim-commentary', keys = {'gc', 'gcc', 'gcu'}}
+  use {'tpope/vim-vinegar', keys = '-'}
+  use {'tpope/vim-dispatch', cmd = {'Make', 'Dispatch', 'Start', 'Spawn'}}
+  use 'dhruvasagar/vim-table-mode'
+  use 'ap/vim-css-color'
+
+  use {
+    'lervag/vimtex',
+    ft = 'tex',
+    config = function()
+      vim.keymap.set('n', '<Leader>cc', '<Cmd>w<CR> <Plug>(vimtex-compile-ss)')
+      vim.keymap.set('n', '<Leader>mc', '<Cmd>w<CR> <Plug>(vimtex-compile)')
+    end
+  }
+
+  if packer_bootstrap then
+    require 'packer'.sync()
+  end
+end)
---
+++ lua/colorscheme.lua
@@ -0,0 +1,14 @@
+vim.opt.termguicolors = true
+
+vim.g.gruvbox_italic = true
+vim.cmd 'colorscheme gruvbox'
+
+local transparent_groups = {
+  Normal = {},
+  Conceal = {},
+  VertSplit = {},
+  SignColumn = {},
+}
+for g, o in pairs(transparent_groups) do
+  vim.cmd(string.format('hi %s guibg=NONE %s', g, table.concat(o, ' ')))
+end
