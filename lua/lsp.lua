7cc5e05b0f21bb299c1d4cf8bb4b28d6d4f469bb
jakobgrine <18680782+jakobgrine@users.noreply.github.com>
2022-12-21 16:17:18 +0100

    Remove sign column and add keybindings

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,6 +1,23 @@
 local lsp = require 'lsp-zero'
 
 lsp.preset 'recommended'
+lsp.set_preferences {
+  sign_icons = {
+    -- error = 'E',
+    -- warn = 'W',
+    -- hint = 'H',
+    -- info = 'I'
+  }
+}
+
+lsp.on_attach(function(_, bufnr)
+  local opts = {buffer = bufnr, remap = false}
+
+  vim.keymap.set('n', '<Leader>ca', function() vim.lsp.buf.code_action() end, opts)
+  vim.keymap.set('n', '<Leader>cr', function() vim.lsp.buf.rename() end, opts)
+  vim.keymap.set({'n', 'v'}, '<Leader>cf', function() vim.lsp.buf.format() end, opts)
+end)
+
 lsp.setup {}
 
 vim.diagnostic.config {
